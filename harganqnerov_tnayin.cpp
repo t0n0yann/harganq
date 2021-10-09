c89638f1ab4a68eeacd4bea195c5ace452e559ed
t0n0yann <89076837+t0n0yann@users.noreply.github.com>
2021-10-09 19:35:37 +0300

    edo

---
+++ harganqnerov_tnayin.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+using namespace std;
+class Harganq
+{
+public:
+    int pativ;
+    int x;
+    int y;
+    Harganq(int a, int b)
+    {
+        this->x = a;
+        this->y = b;
+    }
+    ~Harganq()
+    {
+        cout << "finished" << "\n";
+    }
+    void mecaranq()
+    {
+        for (; this->x <= this->y; this->x++)
+        {
+            cout << pow(x, 2) << "\n";
+        }
+        if (this->y % 2 == 0)
+        {
+            this->pativ = pow(this->y, 2);
+        }
+        else
+        {
+            this->pativ = pow(this->y - 1, 2);
+        }
+        cout << this->pativ << "\n";
+    }
+};
+int main()
+{
+    Harganq oop(1, 5);
+    oop.mecaranq();
+}
+
